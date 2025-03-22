e1ec69a461fd8827028c6ed3734a7ba55e088c50
ERTAsoN <savel.andr.ivanov@gmail.com>
2025-03-22 17:29:09 +0700

    basic

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import store from "@/store";
 
 Vue.use(VueRouter)
 
@@ -8,17 +8,36 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: function () {
+      return import('../views/HomeView.vue');
+    },
+    beforeEnter: ifAuthenticated,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    component: function () {
+      return import('../components/Login.vue');
+    },
+    beforeEnter: ifNotAuthenticated,
   }
 ]
+
+const ifNotAuthenticated = (to, from, next) => {
+  if (!store.getters.isAuthenticated) {
+    next();
+    return;
+  }
+  next('/');
+};
+
+const ifAuthenticated = (to, from, next) => {
+  if (store.getters.isAuthenticated) {
+    next();
+    return;
+  }
+  next('/login');
+}
 
 const router = new VueRouter({
   mode: 'history',
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import loginRequest from '../utils/api.js';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: localStorage.getItem('myAppToken') || '',
   },
   getters: {
+    isAuthenticated: (state) => !!state.token,
   },
   mutations: {
+    AUTH_SUCCESS: (state, token) => {
+      state.token = token;
+    },
+    AUTH_ERROR: (state) => {
+      state.token = '';
+    }
   },
   actions: {
+    AUTH_REQUEST: ({ commit }, user) => {
+      return new Promise((resolve, reject) => {
+        loginRequest(user)
+          .then((token) => {
+            commit('AUTH_SUCCESS', token);
+            localStorage.setItem('myAppToken', token);
+            resolve();
+          })
+          .catch(() => {
+            commit('AUTH_ERROR');
+            localStorage.removeItem('myAppToken');
+            reject();
+          })
+      })
+    }
   },
   modules: {
   }
